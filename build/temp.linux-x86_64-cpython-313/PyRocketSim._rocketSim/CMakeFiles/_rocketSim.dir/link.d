774ff1626c4527b25525c7f66742ce7001905390
Vova <vovarybin237@yandex.ru>
2025-05-09 16:55:28 +0300

    test cibuildwheels9

---
+++ build/temp.linux-x86_64-cpython-313/PyRocketSim._rocketSim/CMakeFiles/_rocketSim.dir/link.d
@@ -0,0 +1,28 @@
+_rocketSim.so: \
+  /home/vova_rybin/anaconda3/envs/Rocket/lib/libgcc_s.so \
+  /home/vova_rybin/anaconda3/envs/Rocket/lib/libgcc_s.so \
+  /home/vova_rybin/anaconda3/envs/Rocket/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libc.so \
+  /home/vova_rybin/anaconda3/envs/Rocket/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libc.so \
+  /home/vova_rybin/anaconda3/envs/Rocket/lib/libgcc_s.so \
+  /home/vova_rybin/anaconda3/envs/Rocket/lib/libgcc_s.so \
+  /tmp/ccPPb3Vl.ltrans0.ltrans.o \
+  /tmp/ccPPb3Vl.ltrans1.ltrans.o \
+  /tmp/ccPPb3Vl.ltrans2.ltrans.o
+
+/home/vova_rybin/anaconda3/envs/Rocket/lib/libgcc_s.so:
+
+/home/vova_rybin/anaconda3/envs/Rocket/lib/libgcc_s.so:
+
+/home/vova_rybin/anaconda3/envs/Rocket/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libc.so:
+
+/home/vova_rybin/anaconda3/envs/Rocket/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libc.so:
+
+/home/vova_rybin/anaconda3/envs/Rocket/lib/libgcc_s.so:
+
+/home/vova_rybin/anaconda3/envs/Rocket/lib/libgcc_s.so:
+
+/tmp/ccPPb3Vl.ltrans0.ltrans.o:
+
+/tmp/ccPPb3Vl.ltrans1.ltrans.o:
+
+/tmp/ccPPb3Vl.ltrans2.ltrans.o:
